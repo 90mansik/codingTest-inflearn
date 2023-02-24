a402c90f4ad17b17b95fed16e627fd8fc7deeea0
mansik <90mansik@gmail.com>
2023-02-25 00:27:40 +0900

    feat : 특정 문자 뒤집기 구현

---
+++ src/com/algorithm/String/ReversString2.java
@@ -0,0 +1,46 @@
+package com.algorithm.String;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ReversString2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String inStr = sc.nextLine();
+        char[] inChars = inStr.toCharArray();
+        char[] result = new char[inChars.length];
+
+        int j = inChars.length - 1;
+
+        for (int i = 0; i < inChars.length; i++) {
+            if (isMatchExp(inChars[i])) {
+                result[i] = inChars[i];
+            } else {
+                boolean flag = true;
+                do {
+                    if (!isMatchExp(inChars[j])) {
+                        result[i] = inChars[j];
+                        flag = false;
+                    }
+                    j--;
+                } while (flag);
+            }
+        }
+
+        System.out.println(String.valueOf(result));
+
+    }
+
+    private static boolean isMatchExp(char ch) {
+        String expression = "[!@#$%^&*(),.?\":{}|<>]";
+        Pattern pattern = Pattern.compile(expression);
+
+        if (pattern.matcher(String.valueOf(ch)).find()) {
+            return true;
+        }
+        return false;
+    }
+
+}
