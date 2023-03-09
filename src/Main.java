f3388881ec651440705ae4bc65f879075769140b
mansik <90mansik@gmail.com>
2023-03-09 23:08:33 +0900

    docs : 코딩테스트 문서 작성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int N = 3;
+        int K = 2;
+
+        HashMap<Character, Integer> hashMap = createRandomAlphabet(K);
+        char[] result = new char[N];
+
+        int endPoint = N - 1;
+        int k = 0;      // 랜덤 알파벳을 가져오기 위함
+        for (int startPoint = 0; startPoint < N / 2; startPoint++) {
+            if(k == hashMap.size()){
+                k = 0;
+            }
+
+            if ( startPoint != endPoint ){
+                result[startPoint] = getAlphabet(hashMap, k);
+                result[endPoint--] = getAlphabet(hashMap, k);
+            }
+
+            if( (startPoint+1) == endPoint){
+                result[startPoint+1] = getAlphabet(hashMap, k+1);
+            }
+
+            k++;
+        }
+
+        System.out.println(Arrays.toString(result));
+
     }
+
+    public static HashMap<Character, Integer> createRandomAlphabet(int alphabetCnt) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        int i = 0;
+        while (alphabetCnt > hashMap.size()) {
+            char ch = (char) ((Math.random() * 26) + 97);
+            if (!hashMap.containsKey(ch)) {
+                hashMap.put(ch, i++);
+            }
+        }
+
+        return hashMap;
+    }
+
+    public static Character getAlphabet(HashMap<Character, Integer> hashMap, int value) {
+        for (Character key : hashMap.keySet()) {
+            // 키와 매핑된 값이랑 equals() 메서드에 전달된 값이랑 일치하면 반복문을 종료합니다.
+            if (hashMap.get(key).equals(value)) { // 키가 null이면 NullPointerException 예외 발생
+                return key;
+            }
+        }
+        return ' ';
+    }
+
 }
